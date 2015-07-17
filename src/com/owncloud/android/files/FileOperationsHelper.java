f6c468d312cda9a5285f499fb8e700f7a9b9fca1
David A. Velasco <dvelasco@solidgear.es>
2015-07-17 14:29:27 +0200

    Join cancelation of downloads and uploads in 'cancel sync', both for files and folders

--- src/com/owncloud/android/files/FileOperationsHelper.java
+++ src/com/owncloud/android/files/FileOperationsHelper.java
@@ -326,7 +326,6 @@ public class FileOperationsHelper {
 
         // for both files and folders
         FileDownloaderBinder downloaderBinder = mFileActivity.getFileDownloaderBinder();
-        FileUploaderBinder uploaderBinder = mFileActivity.getFileUploaderBinder();
         if (downloaderBinder != null && downloaderBinder.isDownloading(account, file)) {
             downloaderBinder.cancel(account, file);
 
@@ -338,7 +337,9 @@ public class FileOperationsHelper {
                 mFileActivity.getStorageManager().saveFile(parent);
             }
 
-        } else if (uploaderBinder != null && uploaderBinder.isUploading(account, file)) {
+        }
+        FileUploaderBinder uploaderBinder = mFileActivity.getFileUploaderBinder();
+        if (uploaderBinder != null && uploaderBinder.isUploading(account, file)) {
             uploaderBinder.cancel(account, file);
         }
     }
